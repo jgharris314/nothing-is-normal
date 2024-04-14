03fe51246c41e67101e699887dd721aa64afb804
Jacob <jgharris314@gmail.com>
2024-04-14 12:22:09 -0600

    updated page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { ParentalIsNoraml } from "./components/ParentalAdvisoryLogo"
 import MemberRow from "./components/MemberRow"
 
-const GapRow = () => <div className="h-[30rem]" />
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col uppercase bg-slate-900 overflow-hidden px-10 pb-32">
